fc7835a76f8d38127c97fa4ffd34fbd1da4332f7
Darius Austin <dariusaustin24@gmail.com>
2019-12-05 14:03:50 -0600

    added more stuff

--- main.py
+++ main.py
@@ -6,6 +6,27 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+@app.route("/welcome", methods=['POST'])
+def welcome():
+    user = request.form['username']
+    if user == user:
+        if user.count() <= 20 and user.count() >= 3:
+            print('Username must be 3-20 charactes')
+        elif " " in user:
+            print("Invalid Username")
+        return redirect("/welcome")
+    return redirect ("/")
+
+    pswd = request.form ['password']
+
+    ver_pswd = request.form["password_confirmation"]
+    if pswd == ver_pswd:
+        return render_template
+    else: 
+        print("Password does not match.")
+    
+
+    email = request.form['email-address']
 
 @app.route("/")
 def index():
