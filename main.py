f49b8695e04249e5cf5d325138823909873571cc
Darius Austin <dariusaustin24@gmail.com>
2019-12-06 13:23:05 -0600

    peridoc save

--- main.py
+++ main.py
@@ -5,29 +5,31 @@ from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        username = request.form['username']
+        if email.isspace() == False:
+            if email.count("@") == 1 and email.count(".") == 1 and email.count(' ') < 0:
+                return redirect('/welcome')
+        return redirect('/')
+                
 
-@app.route("/welcome", methods=['POST'])
-def welcome():
-    user = request.form['username']
-    if user == user:
-        if user.count() <= 20 and user.count() >= 3:
-            print('Username must be 3-20 charactes')
-        elif " " in user:
-            print("Invalid Username")
-        return redirect("/welcome")
-    return redirect ("/")
 
-    pswd = request.form ['password']
+        
+        if password.count() >= 3 and password.count() <= 20:
+            pass
+            
+    return redirect('/login')
+        
 
-    ver_pswd = request.form["password_confirmation"]
-    if pswd == ver_pswd:
-        return render_template
-    else: 
-        print("Password does not match.")
-    
 
-    email = request.form['email-address']
-
+@app.route("/welcome", methods=['POST', 'GET'])
+def welcome(): 
+    return render_template('welcome.html') 
+   
 @app.route("/")
 def index():
     return render_template('functions.html')
